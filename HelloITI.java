b61653ae52d29ed8b17b8f456d996bf8d346cf20
Karam Salama <karamslama244@gmail.com>
2024-11-23 04:02:41 +0200

    add method printHelloGit() in HelloITI.java class

--- HelloITI.java
+++ HelloITI.java
@@ -3,4 +3,7 @@ public class HelloITI {
     public void printHelloITI() {
         System.out.println("Hello ITI");
     }
+    public void printHelloGit() {
+        System.out.println("Hello Git");
+    }
 }
